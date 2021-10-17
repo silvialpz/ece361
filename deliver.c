ec5ab36bb55b2647dd6ca21684284f45cf31472a
Daniel-Yu Chun Chen <danielyu.chen@mail.utoronto.ca>
2021-10-17 18:09:48 -0400

    first commit

---
+++ deliver.c
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+/*
+ * 
+ */
+
+bool numberChecker(char *string){
+    for(int i = 0; i < strlen(string); i++){
+        if(isdigit(string[i]) == 0 && string[i] != '.'){
+            return false;
+        }
+    }
+    return true;
+}
+
+void sendFile(int fd, char* fileName, struct sockaddr_in toAddr){
+    char filedata[1000];
+    unsigned int total_frag;
+    struct sockaddr_storage addrStorage;
+    socklen_t addrStorageSize = sizeof(addrStorage);
+    char buffer[500] = "";
+    
+    FILE* fp = fopen(fileName, "r");
+    if(fp == NULL){
+        fprintf(stderr, "Open file failed\n");
+        exit(1);
+    }
+    
+    //to get the size of the file and the number of fragments
+    fseek(fp, 0, SEEK_END);
+    long int fileLength = ftell(fp);
+    if(fileLength%1000 != 0){
+        total_frag = (fileLength/1000) + 1;             // +1 at the end to account for remainders
+    }else{
+        total_frag = fileLength/1000;
+    }
+    fseek(fp, 0, SEEK_SET);
+    int fileLengthRem = fileLength%1000;
+    
+    struct timeval timeoutVal;
+    timeoutVal.tv_sec = 0;
+    timeoutVal.tv_usec = 1000;
+    
+    if(setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &timeoutVal, sizeof(timeoutVal)) == -1){
+        fprintf(stderr, "Failed to set timeout value on socket\n");
+        exit(0);
+    }
+    
+    char message[2000];
+    int dataAddr;
+    for(int packet_frag = 1; packet_frag <= total_frag; packet_frag++){
+        int sent;        
+        int bytesRead = fread(filedata, sizeof(char), 1000, fp);        
+        if(packet_frag == total_frag){
+            dataAddr = sprintf(message, "%d:%d:%d:%s:", total_frag, packet_frag, fileLengthRem, fileName);
+            memcpy(&message[dataAddr], filedata, fileLengthRem);
+            sent = sendto(fd, message, dataAddr + fileLengthRem, 0, (struct sockaddr*)&toAddr, sizeof(toAddr));
+        }else{
+            dataAddr = sprintf(message, "%d:%d:%d:%s:", total_frag, packet_frag, 1000, fileName);
+            memcpy(&message[dataAddr], filedata, 1000);
+            sent = sendto(fd, message, dataAddr + 1000, 0, (struct sockaddr*)&toAddr, sizeof(toAddr));
+        }
+        
+        if(sent == -1){
+            fprintf(stderr, "Failed to send packet\n");
+            exit(1);
+        }        
+        
+        if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addrStorage, &addrStorageSize) == -1){
+            if(errno == EAGAIN || errno == EWOULDBLOCK){
+                packet_frag--;    
+                fseek(fp, 0 , SEEK_SET);                                            //move the fp pointer to front of file
+                fseek(fp, 1000*packet_frag, SEEK_CUR);                              //move it back to original spot to resend the packet               
+                fprintf(stderr, "Timed out! Timeout = 1000us, frag no = %d\n", packet_frag); 
+            }else{
+                fprintf(stderr, "Receiving failed\n");
+                exit(0);
+            }
+        }
+        
+        strcpy(message, "");
+        strcpy(filedata, "");
+        strcpy(buffer, "");
+    } 
+    fprintf(stderr, "File transfer successful\n");
+} 
+
+
+
+int main(int argc, char** argv) {
+    char *serverAddress;
+    int portNum; 
+    struct sockaddr_in toAddr;
+    struct timeval start, RTT;
+    
+    if(argc != 3){
+        return 0;
+    }else{
+        if(numberChecker(argv[1]) == true){
+            serverAddress = argv[1];
+        }else{     
+            return 0;
+        }      
+        if(numberChecker(argv[2]) == true){
+            portNum = atoi(argv[2]);
+        }else{
+            return 0;
+        }
+    }
+    
+    int fd;
+    if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+        exit(1);
+    }
+    //get input from user
+    
+    fprintf(stderr, "Input a message of the following form\n    ftp <file name>\n");
+    char inputMessage[200], fileName[200];
+    scanf("%s", inputMessage);
+    scanf("%s", fileName);
+    
+    
+    toAddr.sin_family = AF_INET;
+    toAddr.sin_port = htons(portNum);
+    toAddr.sin_addr.s_addr = inet_addr(serverAddress);
+  
+    if(access(fileName, F_OK) != -1){                 //if it opens that means the file exist
+        gettimeofday(&start, NULL);
+        if(sendto(fd, inputMessage, strlen(inputMessage)+1, 0, (struct sockaddr*)&toAddr, sizeof(toAddr)) == -1){
+            fprintf(stderr, "Send fail\n");
+            exit(1);
+        }
+    }else{        
+        fprintf(stderr, "File doesn't exist\n");
+        exit(1);
+    }
+    
+    struct sockaddr_storage addrStorage;
+    socklen_t addrStorageSize = sizeof(addrStorage);
+    char buffer[500] = "";
+    if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addrStorage, &addrStorageSize)==-1){
+        exit(1);
+    }
+    gettimeofday(&RTT, NULL);
+    int RTTus = (RTT.tv_usec - start.tv_usec);
+    fprintf(stderr, "RTT = %d us.\n", RTTus);
+    if(strcmp(buffer, "yes") == 0){
+        fprintf(stderr, "A file transfer can start.\n");
+        sendFile(fd, fileName, toAddr);
+    }else{
+        fprintf(stderr, "Please type ftp before the file name.\n");
+    }
+    
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,219 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: chenda37
+ *
+ * Created on September 21, 2021, 7:23 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+
+/*
+ * 
+ */
+bool numberChecker(char *string){
+    for(int i = 0; i < strlen(string); i++){
+        if(isdigit(string[i]) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char** argv) {
+    srand(time(0));
+    int portNum;
+    if(argc !=2){
+        return 0;
+    }else{
+        if(numberChecker(argv[1]) == true){
+            portNum = atoi(argv[1]);
+        }else{
+            return 0;
+        }
+    }
+    
+    struct sockaddr_in addr;
+    int fd;
+    if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+        exit(1);
+    }
+    
+    addr.sin_family = AF_INET;    
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(portNum);
+    int bd;
+    if((bd = bind(fd, (struct sockaddr*)&addr, sizeof(addr))) == -1){
+        addr.sin_port = htons(0);                                           //let os decide port num 
+        fprintf(stderr, "Connection to port %d failed. Now connected to %d\n", portNum, ntohs(addr.sin_port));
+        if((bd = bind(fd, (struct sockaddr*)&addr, sizeof(addr))) == -1){
+            exit(1);
+        }
+    }
+    char a[100] = "Server receiving on port ";
+    char pmessage[100];
+    sprintf(pmessage, "%d\n", ntohs(addr.sin_port));
+    fprintf(stderr, strcat(a, pmessage));
+    
+    char buffer[2000] = "";
+    char message[500] = "";
+    struct sockaddr_storage addrStorage;
+    socklen_t addrStorageSize = sizeof(addrStorage);
+    while(true){
+        if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addrStorage, &addrStorageSize) == -1){
+            exit(1);
+        }else{
+            if(strcmp(buffer, "ftp") == 0){
+                strcat(message, "yes");             
+                if(sendto(fd, message, sizeof(message), 0, (struct sockaddr*)&addrStorage, addrStorageSize) == -1){
+                    exit(1);
+                }else{
+                    strcpy(message, "");
+                    strcpy(buffer,"");  
+                    break;
+                }
+            }else{
+                strcat(message, "no");
+                if(sendto(fd, message, sizeof(message), 0, (struct sockaddr*)&addrStorage, addrStorageSize) == -1){
+                    exit(1);
+                }
+            }  
+        }
+        strcpy(message, "");
+        strcpy(buffer,"");   
+    }
+    
+    unsigned int total_frag, frag_num, size;
+    char name[200];
+    char filedata[1000];
+    FILE *fp = NULL;
+    bool first = true;
+    while(true){
+        if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addrStorage, &addrStorageSize) == -1){
+            exit(1);  
+        }else{      
+            if(first == true){
+                int colonCount = 0; 
+                int categoryLength = 0;
+                int category_got = 0;
+                char *category = NULL;            
+                for(int i = 0; i < 2000; i++){
+                    if(buffer[i] != ':'){
+                        if(buffer[i+1] != ':'){
+                            categoryLength++;
+                        }else{                  //when we hit :
+                            categoryLength++;
+                            category = malloc(sizeof(char)* (categoryLength + 1));
+                            for(int j = 0; j < categoryLength; j++){
+                                category[j] = buffer[i-categoryLength + 1 + j];
+                            }
+                            category[categoryLength]= '\0';
+                            if(category_got == 0){
+                                total_frag = atoi(category);
+                            }else if(category_got == 1){
+                                frag_num = atoi(category);
+                            }else if(category_got == 2){
+                                size = atoi(category);
+                            }else if(category_got == 3){
+                                strcpy(name, category);
+                            }
+                            category_got++;
+                            categoryLength = 0;
+                        }
+                    }else{
+                        colonCount++;
+                    }
+                    if(colonCount == 4){
+                        memcpy(filedata, &buffer[i+1], size);
+                        break;
+                    }
+                }
+                
+                if(rand()%100 != 1){
+                    strcat(message,"Acknowledged");
+                    if(sendto(fd, message, sizeof(message), 0, (struct sockaddr*)&addrStorage, addrStorageSize) == -1){
+                        exit(1);
+                    }
+                    strcpy(message, "");
+
+                    fp = fopen(name, "w");            
+                    if(fp == NULL){
+                        fprintf(stderr, "Error creating file\n");
+                        exit(1);
+                    }  
+
+                    fwrite(filedata, sizeof(char), size, fp);
+
+                    first = false;
+                }else{
+                    fprintf(stderr, "Packet dropped\n");
+                }
+            }else{
+                int colonCount = 0; 
+                int categoryLength = 0;
+                char *category = NULL;            
+                for(int i = 0; i < 2000; i++){
+                    if(buffer[i] != ':'){
+                        if(buffer[i+1] != ':'){
+                            categoryLength++;
+                        }else{                  //when we hit :
+                            categoryLength++;
+                            category = malloc(sizeof(char)* (categoryLength + 1));
+                            for(int j = 0; j < categoryLength; j++){
+                                category[j] = buffer[i-categoryLength + 1 + j];
+                            }
+                            category[categoryLength]= '\0';
+                           
+                            if(colonCount == 1){
+                                frag_num = atoi(category);
+                            }else if(colonCount == 2){
+                                size = atoi(category);
+                            }
+                            categoryLength = 0;
+                        }
+                    }else{
+                        colonCount++;
+                    }
+                    if(colonCount == 4){
+                        memcpy(filedata, &buffer[i+1], size);
+                        break;
+                    }
+                }
+                
+                if(rand()%100 != 1){
+                    strcat(message, "Acknowledged");
+                    if(sendto(fd, message, sizeof(message), 0, (struct sockaddr*)&addrStorage, addrStorageSize) == -1){
+                        exit(1);
+                    }              
+                    strcpy(message, "");
+
+                    fwrite(filedata, sizeof(char), size, fp);
+                }else{                   
+                    fprintf(stderr, "Packet dropped\n");  
+                }
+            }
+            if(frag_num == total_frag){
+                break;
+            }
+        }
+        
+    }
+    
+    return 0;
+}
+
+
